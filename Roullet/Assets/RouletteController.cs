cee3f049dd3c2877953a0b813a1ec828f7abeefc
sikk806 <sikk806@gmail.com>
2020-07-03 19:52:29 +0900

    Second Unity

---
+++ Roullet/Assets/RouletteController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    float spinSpeed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 마우스 클릭
+        if(Input.GetMouseButtonDown(0))
+        {
+            this.spinSpeed = 10;
+        }
+
+        transform.Rotate(0, 0, this.spinSpeed);
+
+        this.spinSpeed *= 0.996f;
+    }
+}
---
+++ Sample/Assets/Test.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player
+{
+    private int hp = 100;
+    private int power = 20;
+
+    public void Attack()
+    {
+        Debug.Log(this.power + "데미지를 입혔다!!!");
+    }
+
+    public void Damage(int damage)
+    {
+        this.hp -= damage;
+        Debug.Log(damage + "데미지를 입었다!!!");
+    }
+}
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 캐릭터의 첫 위치라고 생각
+        Player myPlayer = new Player();
+        myPlayer.Attack();
+        myPlayer.Damage(30);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 캐릭터가 start이후에 계속 움직이는 것을 표시
+    }
+}
